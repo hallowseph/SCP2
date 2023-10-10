d38a210898d6dd5ce3666e5f9c85d019fa97d53d
hallowseph <146803230+hallowseph@users.noreply.github.com>
2023-10-10 22:03:09 +1300

    Update InventoryManagementSystemGUI.java

--- src/scp2/InventoryManagementSystemGUI.java
+++ src/scp2/InventoryManagementSystemGUI.java
@@ -2,7 +2,6 @@ package scp2;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
