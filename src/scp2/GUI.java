77944d46e427594cb944d8baeb34164de9728d64
hallowseph <146803230+hallowseph@users.noreply.github.com>
2023-10-10 18:06:25 +1300

    Inventory Management System GUI
    updated GUI for Inventory Management System

--- src/scp2/GUI.java
+++ src/scp2/GUI.java
@@ -4,29 +4,69 @@
  */
 package scp2;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
- * @author josep
+ * @author xxg8089
  */
 public class GUI {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        JButton button = new JButton("Click here");
+        //Main frame
         JFrame frame = new JFrame();
-        
-        frame.setTitle("JFrame1");
-        frame.setSize(400,200);
-        frame.setLocation(300,100);
+        frame.setTitle("Inventory Management System");
+        frame.setSize(700,600);
+        frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(button);
-        frame.setVisible(true);
+        frame.setLayout(new BorderLayout());
         
+        //Panel for welcome message and options
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
+        
+        //Label for welcome message
+        JLabel welcomeLabel = new JLabel("Welcome to the Inventory Management System");
+        welcomeLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        
+        //Buttons for the options
+        JButton displayButton = new JButton("1.Display current stock");
+        JButton addButton = new JButton("2.Add product type");
+        JButton removeButton = new JButton("3.Remove product type");
+        JButton buyButton = new JButton("4.Buy product (Acquire more of am existing product");
+        JButton sellButton = new JButton("5.Sell product (Sell a relevant quantity of an existing product");
+        JButton exitButton = new JButton("6.Exit program");
+        
+        //Action listeners to the buttons
+        /*
+        
+        
+        
+        
+        
+        
+        */
+        
+        //Add components to the panel
+        panel.add(welcomeLabel);
+        panel.add(displayButton);
+        panel.add(buyButton);
+        panel.add(removeButton);
+        panel.add(buyButton);
+        panel.add(sellButton);
+        panel.add(exitButton);
+        
+        //Add the panel to the frame
+        frame.add(panel,BorderLayout.CENTER);
+        
+        //Make the frame visible
+        frame.setVisible(true);
+
     }
     
 }
