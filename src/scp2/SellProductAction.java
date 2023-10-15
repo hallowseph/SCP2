951bbd2ad3a3fe8d35028aa4cf1830f0a17baaab
hallowseph <146803230+hallowseph@users.noreply.github.com>
2023-10-16 01:09:36 +1300

    Update SellProductAction.java
    functional sell product button, asks the user for the product name, if it exists then user will input the quantity to be sold.

--- src/scp2/SellProductAction.java
+++ src/scp2/SellProductAction.java
@@ -2,6 +2,8 @@ package scp2;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
 
 /**
  *
@@ -9,10 +11,41 @@ import java.awt.event.ActionListener;
  * @author xxg8089
  */
 public class SellProductAction implements ActionListener {
-
+    private InventoryManager inventoryManager;
+    private JTextArea textArea;
+    
+    public SellProductAction(InventoryManager inventoryManager, JTextArea textArea){
+        this.inventoryManager = inventoryManager;
+        this.textArea = textArea;
+    }
+    
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("5.Sell product (Sell a relevant quantity of an existing product)");
+        String productName = JOptionPane.showInputDialog("Enter product name to sell:");
+        if(productName != null && !productName.isEmpty()){
+            Product existingProduct = inventoryManager.findProductByName(productName);
+            if(existingProduct != null){
+                String quantityString = JOptionPane.showInputDialog("Enter quantity to sell:");
+                if(quantityString != null && !quantityString.isEmpty()){
+                    try{
+                        int quantity = Integer.parseInt(quantityString);
+                        
+                        if(quantity > 0) { 
+                            inventoryManager.sellProduct(existingProduct, quantity);
+                            
+                            //Save the stock data to the text file
+                            inventoryManager.saveStockToFile();
+                        } else{
+                            JOptionPane.showMessageDialog(null, "Quantity must be greater than 0.", "Sell Product Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                    }catch (NumberFormatException ex){
+                        JOptionPane.showMessageDialog(null, "Invalid quantity. Please enter a valid integer.", "Sell Product Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                } 
+            }
+        } else{
+            JOptionPane.showMessageDialog(null, "Product not found in stock.", "Sell Product Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
     
 }
