ba4f227f6134f74a04440f9493bf3238290f1f32
hallowseph <146803230+hallowseph@users.noreply.github.com>
2023-10-17 14:30:32 +1300

    Update BuyProductAction.java
    added appropriate error handling for the buy product, makes sure that the user input is not empty, makes sure that the quantity is also valid, it also asks the user again until a valid quantity is entered. also did modifications to the gui by removing the numbers on the buttons to make it more tidy.

--- src/scp2/BuyProductAction.java
+++ src/scp2/BuyProductAction.java
@@ -9,39 +9,79 @@ import javax.swing.JTextArea;
  *
  * @author josep
  * @author xxg8089
- * 
+ *
  */
-public class BuyProductAction implements ActionListener{
+public class BuyProductAction implements ActionListener {
+
     private InventoryManager inventoryManager;
     private JTextArea textArea;
-    
-    public BuyProductAction(InventoryManager inventoryManager, JTextArea textArea){
+
+    public BuyProductAction(InventoryManager inventoryManager, JTextArea textArea) {
         this.inventoryManager = inventoryManager;
         this.textArea = textArea;
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
         String productName = JOptionPane.showInputDialog("Enter product name to buy:");
-        if(productName != null && !productName.isEmpty()){
-            Product existingProduct = inventoryManager.findProductByName(productName);
-            if(existingProduct != null){
+        while (true) {
+            productName = JOptionPane.showInputDialog("Enter product name to buy:");
+
+            if (productName == null) {
+                //if user cancels input, exit the loop
+                return;
+            }
+
+            if (productName.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Product name cannot be empty.", "Input Error", JOptionPane.ERROR_MESSAGE);
+            } else if (isInteger(productName)) {
+                JOptionPane.showMessageDialog(null, "Product name cannot be an integer.", "Input Error", JOptionPane.ERROR_MESSAGE);
+            } else {
+                break;//if user input is valid, exit loop
+            }
+        }
+
+        Product existingProduct = inventoryManager.findProductByName(productName);
+        if (productName != null) {
+            while (true) {
                 String quantityString = JOptionPane.showInputDialog("Enter quantity to buy:");
-                if(quantityString != null && !quantityString.isEmpty()){
-                    try{
+
+                if (quantityString == null) {
+                    //if user cancels input, exit the loop
+                    return;
+                }
+
+                if (quantityString.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Quantity cannot be empty.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    try {
                         int quantity = Integer.parseInt(quantityString);
-                        inventoryManager.buyProduct(existingProduct, quantity);
-                        
-                        //Save the stock data to text files
-                        inventoryManager.saveStockToFile();
-                    }catch (NumberFormatException ex){
+
+                        if (quantity > 0) {
+                            inventoryManager.buyProduct(existingProduct, quantity);
+
+                            //save the stock data to text files
+                            inventoryManager.saveStockToFile();
+                            break; //if valid quantity, exit the loop 
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Quantity must be greater than 0.", "Buy Product Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                    } catch (NumberFormatException ex) {
                         JOptionPane.showMessageDialog(null, "Invalid quantity. Please enter a valid integer.", "Buy Product Error", JOptionPane.ERROR_MESSAGE);
                     }
                 }
-            }else{
-                JOptionPane.showMessageDialog(null, "Product not found in stock.", "Buy Product Error", JOptionPane.ERROR_MESSAGE);
             }
+        } else {
+            JOptionPane.showMessageDialog(null, "Product not found in stock.", "Buy Product Error", JOptionPane.ERROR_MESSAGE);
         }
     }
-    
+
+    private boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
