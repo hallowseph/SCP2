feee3d2d98551aec97e8804eb2c23edb58b81185
hallowseph <146803230+hallowseph@users.noreply.github.com>
2023-10-17 13:31:19 +1300

    Update AddProductAction.java
    modified so that it wont accept integers as the product name

--- src/scp2/AddProductAction.java
+++ src/scp2/AddProductAction.java
@@ -13,6 +13,7 @@ import javax.swing.JTextArea;
  * @author xxg8089
  */
 public class AddProductAction implements ActionListener {
+
     private InventoryManager inventoryManager;
     private JTextArea textArea;
 
@@ -23,54 +24,8 @@ public class AddProductAction implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String selectedProductType = selectProductType();
-
-        if (selectedProductType != null) {
-            String productName = JOptionPane.showInputDialog("Enter product name:");
-            while (productName != null && productName.isEmpty()) {
-                JOptionPane.showMessageDialog(null, "Product name cannot be empty.", "Input Error", JOptionPane.ERROR_MESSAGE);
-                productName = JOptionPane.showInputDialog("Enter product name:");
-            }
-
-            if (productName != null) {
-                Product productToAdd = null;
-
-                if (selectedProductType.equals("Grocery")) {
-                    productToAdd = new GroceryProduct(productName, new Date(), 0);
-                } else if (selectedProductType.equals("Electronics")) {
-                    String skuCode = JOptionPane.showInputDialog("Enter SKU code:");
-                    while (skuCode != null && skuCode.isEmpty()) {
-                        JOptionPane.showMessageDialog(null, "SKU code cannot be empty.", "Input Error", JOptionPane.ERROR_MESSAGE);
-                        skuCode = JOptionPane.showInputDialog("Enter SKU Code:");
-                    }
-                    
-                    if (skuCode != null) {
-                        productToAdd = new ElectronicsProduct(productName, skuCode, 0);
-                    }
-                }
-
-                if (productToAdd != null) {
-                    inventoryManager.addProduct(productToAdd);
-
-                    //Display the newly added product
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
-
-                    //Save the stock data to text files
-                    inventoryManager.saveStockToFile();
-
-                    //Message dialog
-                    JOptionPane.showMessageDialog(null,
-                            productName + " added to stock.\nGrocery stock data saved to GroceryStock.txt\nElectronics stock data saved to ElectronicsStock.txt",
-                            "Product Added",
-                            JOptionPane.INFORMATION_MESSAGE);
-                }
-            }
-        }
-    }
-
-    private String selectProductType() {
         String[] productTypes = {"Grocery", "Electronics"};
-        return (String) JOptionPane.showInputDialog(
+        String selectedProductType = (String) JOptionPane.showInputDialog(
                 null,
                 "Select a product type:",
                 "Add Product",
@@ -79,5 +34,61 @@ public class AddProductAction implements ActionListener {
                 productTypes,
                 productTypes[0]
         );
+
+        if (selectedProductType != null) {
+            String productName = null;
+            while (true) {
+                productName = JOptionPane.showInputDialog("Enter product name:");
+
+                if (productName == null) {
+                    //if user cancels the input, exit the loop
+                    return;
+                }
+
+                if (productName.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Product name cannot be empty.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                } else if (isInteger(productName)) {
+                    JOptionPane.showMessageDialog(null, "Product name cannot be an integer.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    break;// if valid product name, exit loop
+                }
+            }
+
+            Product productToAdd = null;
+
+            if (selectedProductType.equals("Grocery")) {
+                productToAdd = new GroceryProduct(productName, new Date(), 0);
+            } else if (selectedProductType.equals("Electronics")) {
+                String skuCode = JOptionPane.showInputDialog("Enter SKU code:");
+                if (skuCode != null && !skuCode.isEmpty()) {
+                    productToAdd = new ElectronicsProduct(productName, skuCode, 0);
+                }
+            }
+
+            if (productToAdd != null) {
+                inventoryManager.addProduct(productToAdd);
+
+                //Display the newly added product
+                SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
+
+                //Save the stock data to text files
+                inventoryManager.saveStockToFile();
+
+                //Message dialog
+                JOptionPane.showMessageDialog(null,
+                        productName + " added to stock.\nGrocery stock data saved to GroceryStock.txt\nElectronics stock data saved to ElectronicsStock.txt",
+                        "Product Added",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        }
+    }
+
+    private boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
     }
 }
